e7629599ab4ce4c166632342bc10e8dd690cb739
Yehor Prykhodko <350zegor@gmail.com>
2023-03-31 10:42:40 +0300

    new com

--- github test/src/Fichier.java
+++ github test/src/Fichier.java
@@ -7,5 +7,16 @@ public class Fichier {
         pw.println(phrase);
         pw.close();
     }
+
+    public static void main(String[] args) throws IOException {
+        Fichier.ecrireFichier("newF.txt", "la phrase");
+        Fichier.readFile("newF.txt");
+    }
+
+    public static void readFile(String nomFich) throws IOException {
+        BufferedReader fich =  new BufferedReader(new FileReader(nomFich));
+        String line = fich.readLine();
+        System.out.println(line);
+    }
     
 }
